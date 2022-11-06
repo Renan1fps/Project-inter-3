f18cbb43ddc3f7a151cc0fef8b15eeb6ead8fb6c
Renan1fps <renan.sergipe68@gmail.com>
2022-11-06 19:51:35 -0300

    refactor: remove duplicated field

--- schema.sql
+++ schema.sql
@@ -16,13 +16,11 @@ create table if not exists tb_unit(
   PRIMARY KEY (id_unit)
 );
 
-
 create table if not exists tb_car(
 	id_unit INT AUTO_INCREMENT,
 	id_unidade INT,
   	marca VARCHAR(100),
   	modelo VARCHAR(100),
-  	tipo VARCHAR(50),
   	ano INT,
   	cor VARCHAR(50),
   	valor FLOAT,
