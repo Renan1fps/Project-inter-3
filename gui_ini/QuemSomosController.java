72c90e37ff5c0b24e1be67380c8d350ba69f3043
Renan1fps <renan.sergipe68@gmail.com>
2022-11-15 11:06:59 -0300

    refactor: add style onHover buttom and navigate pages

---
+++ gui_ini/QuemSomosController.java
@@ -0,0 +1,30 @@
+package gui_ini;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import utils.Loader;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuemSomosController implements Initializable {
+
+    private Loader loader;
+
+    @FXML
+    public void handleClickServices() {
+        loader.loadView("/gui_ini/servicos.fxml", (x -> {
+        }));
+    }
+
+    @FXML
+    public void handleClickInit() {
+        loader.loadView("/gui_ini/inicio.fxml", (x -> {
+        }));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loader = new Loader();
+    }
+}
