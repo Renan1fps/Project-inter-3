7bcd5090c81a82829e2f5b3f00f81f249163ffff
Renan1fps <renan.sergipe68@gmail.com>
2022-11-20 18:44:06 -0300

    refactor: change script db

--- schema.sql
+++ schema.sql
@@ -1,28 +1,42 @@
+drop database  projeto;
+
+create database if not exists projeto;
 use projeto;
+
+drop table tb_user;
 
 create table if not exists tb_user(
   id_user INT AUTO_INCREMENT,
-  name VARCHAR(100),
-  cpf VARCHAR(11),
-  age INT,
-  createdAt DATETIME,
-  password VARCHAR(255),
+  phone varchar(15) not null,
+  name VARCHAR(100) not null,
+  email VARCHAR(100) not null,
+  nacionalidade varchar(100) not null,
+  cpf VARCHAR(11) not null,
+  password VARCHAR(255) not null,
+  is_admin boolean default 0,
   PRIMARY KEY (id_user)
 );
 
+
 create table if not exists tb_unit(
   id_unit INT AUTO_INCREMENT,
-  name VARCHAR(100),
+  name VARCHAR(100) not null,
+  phoneUni varchar(15) not null,
+  adress varchar(200) not null,
+  city varchar(150) not null,
+  number int not null,
+  state varchar (100) not null, 
   PRIMARY KEY (id_unit)
 );
 
+
 create table if not exists tb_car(
-	id_unit INT AUTO_INCREMENT,
-	id_unidade INT,
-  	marca VARCHAR(100),
+	id_car INT AUTO_INCREMENT,
+	id_unit INT,
   	modelo VARCHAR(100),
   	ano INT,
   	cor VARCHAR(50),
+  	placa varchar (10),
   	valor FLOAT,
   	vidro_eletrico BOOLEAN,
   	cambio_automatico BOOLEAN,
@@ -32,6 +46,9 @@ create table if not exists tb_car(
   	direcao_hidraulica BOOLEAN,
   	porta_mala_grande BOOLEAN,
   	premium BOOLEAN,
-  	PRIMARY KEY (id_unit),
-  	FOREIGN KEY (id_unidade) REFERENCES tb_unit(id_unit)
+  	PRIMARY KEY (id_car),
+  	FOREIGN KEY (id_unit) REFERENCES tb_unit(id_unit)
 );
+
+ALTER TABLE tb_car 
+ADD disponivel boolean default 1; 
